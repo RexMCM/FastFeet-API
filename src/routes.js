ec617064b4fb8689a4bbb3e02153ea44d87c70b5
Matheus Cardoso de Medeiros <Matheus.Cardoso@ibm.com>
2020-02-07 17:11:45 -0300

    user controllet added

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import userController from './app/controllers/userController';
 
 const routes = new Router();
+
+routes.post('/users', userController.store);
 
 export default routes;
