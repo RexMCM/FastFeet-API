6aa66d985551e5d9e8fd28b036e30f146f69d61d
Matheus Cardoso de Medeiros <Matheus.Cardoso@ibm.com>
2020-02-17 10:44:33 -0300

    notification email started

--- src/app/controllers/ordersController.js
+++ src/app/controllers/ordersController.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup';
 import orders from '../models/orders';
 import Deliveryman from '../models/deliveryMan';
 import Recipient from '../models/recipient';
+import Notification from '../schemas/Notification';
 
 class OrdersController {
   async store(req, res) {
@@ -37,10 +38,18 @@ class OrdersController {
     if (!checkDeliverymanExists) {
       return res.status(400).json({ error: 'Deliveryman does not exists' });
     }
+
+    const { name, email } = checkDeliverymanExists;
+
     const order = await orders.create({
       product,
       deliveryman_id,
       recipient_id,
+    });
+
+    await Notification.create({
+      content: `Nova encomenda! Produto: ${product} pronto para retirada`,
+      user: deliveryman_id,
     });
 
     return res.json(order);
